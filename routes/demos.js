ab70737385c34cbeeff8d3a4cafa8c6d00b43b09
lcwben <sizailcw@qq.com>
2017-12-25 17:58:48 +0800

    1.Update demostyle.css. 2.Cut pictuers. 3.Add mock data in demos.js.

--- routes/demos.js
+++ routes/demos.js
@@ -7,7 +7,40 @@ var data = {
     feedback : '暂无',
     maintainer : '测试装维人员',
     phone : '13900012345',
-    maintain_department : '装维部门1'
+    maintain_department : '装维部门1',
+    success : true,
+    fail : false,
+    flow_details : [
+        {
+            segment : '前台受理成功',
+            date : '2017-12-18',
+            time : '10:20:30'
+        },{
+            segment : 'CRM派PBOSS',
+            date : '2017-12-19',
+            time : '11:24:20'
+        },{
+            segment : 'PBOSS派SG',
+            date : '2017-12-20',
+            time : '13:05:11'
+        },{
+            segment : 'SG管线资源配置',
+            date : '2017-12-21',
+            time : '14:56:09'
+        },{
+            segment : 'SG预约上门',
+            date : '2017-12-22',
+            time : '17:08:55'
+        },{
+            segment : 'SG外线施工',
+            date : '2017-12-24',
+            time : '20:14:06'
+        },{
+            segment : '施工归档',
+            date : '2017-12-25',
+            time : '21:50:13'
+        }
+    ]
 }
 
 router.get('/demo1',(req,res,next)=> {
